a78773a63dc811e6bd5d9688a67f8dda46aa72c2
jared.mckneely@gmail.com <jared.mckneely@gmail.com>
2023-09-17 14:40:03 -0600

    Updated comments in LorenzAttractorHelper.cpp

--- hw2/LorenzAttractorHelper.cpp
+++ hw2/LorenzAttractorHelper.cpp
@@ -13,28 +13,28 @@ LorenzAttractorHelper::LorenzAttractorHelper() { }
 // Destructor
 LorenzAttractorHelper::~LorenzAttractorHelper() { }
 
-// display()
+// display() member function
 // Primary OpenGL display function
 // Callback for glutDisplayFunc()
 void LorenzAttractorHelper::display() {
   std::cout << "LorenzAttractorHelper::reshape(): enter function" << std::endl;
 }
 
-// reshape()
+// reshape() member function
 // Primary OpenGL window resize function
 // Callback for glutReshapeFunc()
 void LorenzAttractorHelper::reshape(int w, int h) {
   std::cout << "LorenzAttractorHelper::reshape(): enter function" << std::endl;
 }
 
-// display()
+// display() member function
 // Primary OpenGL arrow key handler function
 // Callback for glutSpecialFunc()
 void LorenzAttractorHelper::special(int key, int x, int y) {
   std::cout << "LorenzAttractorHelper::reshape(): enter function" << std::endl;
 }
 
-// display()
+// display() member function
 // Primary OpenGL keyboard handler function
 // Callback for glutKeyboardFunc()
 void LorenzAttractorHelper::key(unsigned char ch, int x, int y) {
