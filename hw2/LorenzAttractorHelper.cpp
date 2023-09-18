6b579ffd5e30960555502cbd1c220059566b4a68
jared.mckneely@gmail.com <jared.mckneely@gmail.com>
2023-09-17 19:16:33 -0600

    Added iteration for displaying Lorenz Attractor

--- hw2/LorenzAttractorHelper.cpp
+++ hw2/LorenzAttractorHelper.cpp
@@ -8,11 +8,11 @@
  #include <LorenzAttractorHelper.h>
 
 // Globals
-int th = 0;              // Azimuth of view angle
-int ph = 0;              // Elevation of view angle
-int mode = 1;            // Dimension (1-4)
-double z = 0;            // Z variable
-double w = 1;            // W variable
+int th = 50;    // Azimuth of view angle
+int ph = 50;    // Elevation of view angle
+int mode = 1;   // Dimension (1-4)
+double z = 0.0; // Z variable
+double w = 1.0; // W variable
 const double S = 10;
 const double B = 2.6666;
 const double R = 28;
@@ -29,27 +29,36 @@ LorenzAttractorHelper::~LorenzAttractorHelper() { }
 // Callback for glutDisplayFunc()
 void LorenzAttractorHelper::display() {
   // Initialize the scene; set initial viewing angle
+  std::cout << "LorenzAttractorHelper::display(): entrance" << std::endl;
   glClear(GL_COLOR_BUFFER_BIT);
   glLoadIdentity();
   glRotated(ph, 1, 0, 0);
   glRotated(th, 0, 1, 0);
 
   // Set white lines and draw x, y, z axes
-  glColor3ub(255, 255, 255);
+  glColor3f(1, 1, 1);
+  glLineWidth(1.0);
   glBegin(GL_LINES);
-  glVertex3d(0, 0, 0);
-  glVertex3d(1, 0, 0);
-  glVertex3d(0, 0, 0);
-  glVertex3d(0, 1, 0);
-  glVertex3d(0, 0, 0);
-  glVertex3d(0, 0, 1);
+  glVertex3d(0.0, 0.0, 0.0);
+  glVertex3d(1.0, 0.0, 0.0);
+  glVertex3d(0.0, 0.0, 0.0);
+  glVertex3d(0.0, 1.0, 0.0);
+  glVertex3d(0.0, 0.0, 0.0);
+  glVertex3d(0.0, 0.0, 1.0);
+  glEnd();
 
   // Draw the Lorenz Attractor
-  glColor3ub(127, 50, 70);
-  double x = 1;
-  double y = 1;
-  double z = 1;
-  computeEulerStep(x, y, z);
+  double x = 1.0;
+  double y = 1.0;
+  double z = 1.0;
+  glColor3f(0.0f, 0.5f, 0.5f);
+  glLineWidth(3.0);
+  glBegin(GL_LINES);
+  for (int i = 0; i < 50000; i++) {
+    glVertex4f(x, y, z, i);
+    computeEulerStep(x, y, z);
+  }
+  glEnd();
 
   // Flush and swap buffers
   glFlush();
@@ -60,20 +69,38 @@ void LorenzAttractorHelper::display() {
 // Primary OpenGL window resize function
 // Callback for glutReshapeFunc()
 void LorenzAttractorHelper::reshape(int w, int h) {
-  std::cout << "LorenzAttractorHelper::reshape(): enter function" << std::endl;
+  std::cout << "LorenzAttractorHelper::reshape(): entrance" << std::endl; 
 }
 
 // display() member function
 // Primary OpenGL arrow key handler function
 // Callback for glutSpecialFunc()
 void LorenzAttractorHelper::special(int key, int x, int y) {
-  std::cout << "LorenzAttractorHelper::reshape(): enter function" << std::endl;
+  std::cout << "LorenzAttractorHelper::special(): entrance" << std::endl;
+  switch(key) {
+    case GLUT_KEY_RIGHT:
+      th += 5;
+      break;
+    case GLUT_KEY_LEFT:
+      th -= 5;
+      break;
+    case GLUT_KEY_UP:
+      ph += 5;
+      break;
+    case GLUT_KEY_DOWN:
+      ph -= 5;
+      break;
+  }
+  th %= 360;
+  ph %= 360;
+  glutPostRedisplay();
 }
 
 // display() member function
 // Primary OpenGL keyboard handler function
 // Callback for glutKeyboardFunc()
 void LorenzAttractorHelper::key(unsigned char ch, int x, int y) {
+  std::cout << "LorenzAttractorHelper::key(): entrance" << std::endl;
   switch(ch) {
     // ESC = exit
     case 27:
@@ -103,4 +130,5 @@ void LorenzAttractorHelper::computeEulerStep(double &x, double &y, double &z) {
   x += (DT * (S * (y - x)));
   y += (DT * ((x * (R - z)) - y));
   z += (DT * ((x * y) - (B * z)));
+
 }
