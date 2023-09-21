cd5b9d2b562f1c87d8b20f3698debf01af7d61d1
jared.mckneely@gmail.com <jared.mckneely@gmail.com>
2023-09-21 10:49:36 -0600

    Initial commit for hw3 directory

---
+++ hw3/hw3.cpp
@@ -0,0 +1,14 @@
+/*
+ * File: hw2.cpp
+ * Author: Jared McKneely
+ * Description: Main runtime for Homework #2 (CSCI 5229)
+ */
+
+#include <stdio.h>
+#include <iostream>
+
+int main(int arc,char* argv[])
+{
+   std::cout << "Hello World!" << std::endl;
+   return 0;
+}
