1102d8a4ee927a67862860607f9dbe6b269f29c4
jared.mckneely@gmail.com <jared.mckneely@gmail.com>
2023-09-28 17:32:13 -0600

    Created hw4 directory; created hw4.cpp and Makefile

---
+++ hw4/hw4.cpp
@@ -0,0 +1,11 @@
+// File: hw4.cpp
+// Author: Jared McKneely
+// Description: Main runtime for Homework #4 (CSCI 5229)
+
+#include <iostream>
+
+int main(int arc,char* argv[])
+{
+   std::cout << "Hello World!" << std::endl;
+   return 0;
+}
