9e5efbdbbfb27daca0f1949348076800c55fdb00
jared.mckneely@gmail.com <jared.mckneely@gmail.com>
2023-10-04 17:42:15 -0600

    Initialized hw5 directory; added basic Makefile and hw5.cpp file

---
+++ hw5/hw5.cpp
@@ -0,0 +1,15 @@
+// File: hw5.cpp
+// Author: Jared McKneely
+// Description: Main runtime for Homework #5 (CSCI 5229)
+
+#include <iostream>
+
+// Main
+int main(int argc,char* argv[]) {
+  // Enter program
+  std::cout << "hw5::main(): starting program" << std::endl;
+
+  // Exit program
+  std::cout << "hw5::main(): exiting program" << std::endl;
+  return 0;
+}
