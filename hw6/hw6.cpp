5d13ba824e4557cb145b78a723608fc9da2b7e73
jared.mckneely@gmail.com <jared.mckneely@gmail.com>
2023-10-11 21:53:06 -0600

    Initial hw6 commit

---
+++ hw6/hw6.cpp
@@ -0,0 +1,11 @@
+// File: hw6.cpp
+// Author: Jared McKneely
+// Description: Main runtime for Homework #6 (CSCI 5229)
+
+#include <iostream>
+
+// Main
+int main() {
+  std::cout << "Hello world!" << std::endl;
+  return 0;
+}
